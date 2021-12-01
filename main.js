c3a9bc7aeee2e837579394f53416468516ddb7dc
Usong Cho <silvermouse4012@gmail.com>
2021-11-30 20:18:07 -0800

    implemented query for full screen

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const moreBtn = document.querySelector('.moreBtn');
 const infoTitle = document.querySelector('.info-title');
 
-moreBtn.addEventListener('click', e => {
+moreBtn.addEventListener('click', () => {
 	infoTitle.classList.toggle('line-clamp');
 });
