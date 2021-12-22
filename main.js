b489d9059221284a9d8d2c544a961017a91768c1
Usong Cho <silvermouse4012@gmail.com>
2021-12-21 19:39:10 -0800

    implemented the side menu toggle

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
 const moreBtn = document.querySelector('.moreBtn');
 const infoTitle = document.querySelector('.info-title');
+const menuBars = document.querySelector('.menu-bars');
+const sideMenu = document.querySelector('.side-menu');
+
+menuBars.addEventListener('click', () => {
+	sideMenu.classList.toggle('active');
+});
 
 moreBtn.addEventListener('click', () => {
 	infoTitle.classList.toggle('line-clamp');
