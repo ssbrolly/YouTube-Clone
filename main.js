b35d33db54dc34d98f1f48c97ef6b6fa67ef61bc
Usong Cho <silvermouse4012@gmail.com>
2021-12-27 16:54:56 -0800

    implemented scrollable categories nav bar

--- main.js
+++ main.js
@@ -2,9 +2,11 @@ const moreBtn = document.querySelector('.moreBtn');
 const infoTitle = document.querySelector('.info-title');
 const menuBars = document.querySelector('.menu-bars');
 const sideMenu = document.querySelector('.side-menu');
+const categories = document.querySelector('.categories');
 
 menuBars.addEventListener('click', () => {
 	sideMenu.classList.toggle('active');
+	categories.classList.toggle('active');
 });
 
 moreBtn.addEventListener('click', () => {
