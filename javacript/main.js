ef2fe84d55cb59df8f91ca5d098ceb02e41de21d
Usong Cho <silvermouse4012@gmail.com>
2022-01-04 18:19:50 -0800

    started on individual video UI

--- javacript/main.js
+++ javacript/main.js
@@ -6,16 +6,31 @@ const categories = document.querySelector('.categories');
 // Horizontal scrolling //
 const categoriesUl = document.querySelector('.categories-ul');
 
+// Video //
+const videos = document.querySelector('.videos');
+
 menuBars.addEventListener('click', () => {
 	sideMenu.classList.toggle('active');
 	categories.classList.toggle('active');
 });
 
+// categories.addEventListener('mouseover', e => {
+// 	e.preventDefault();
+// 	categories.style.position = 'inherit';
+// 	videos.style.marginTop = '0rem';
+// });
+
+// categories.addEventListener('mouseleave', e => {
+// 	e.preventDefault();
+// 	categories.style.position = 'fixed';
+// 	// video.style.marginTop = '5.6rem';
+// });
+
 categoriesUl.addEventListener('wheel', e => {
 	e.preventDefault();
 	categoriesUl.scrollLeft += e.deltaY;
-	if (e) {
-		categories.style.position = 'fixed';
-	}
-	categories.style.position = 'inherit';
+	// if (e) {
+	// 	categories.style.position = 'fixed';
+	// }
+	// categories.style.position = 'inherit';
 });
