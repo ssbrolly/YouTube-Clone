60f2b51c5198fa4be29bacd87e74fe9720dfc252
Usong Cho <silvermouse4012@gmail.com>
2022-01-18 19:45:59 -0800

    removed all the unecessary commented elements

--- javacript/main.js
+++ javacript/main.js
@@ -14,23 +14,7 @@ menuBars.addEventListener('click', () => {
 	categories.classList.toggle('active');
 });
 
-// categories.addEventListener('mouseover', e => {
-// 	e.preventDefault();
-// 	categories.style.position = 'inherit';
-// 	videos.style.marginTop = '0rem';
-// });
-
-// categories.addEventListener('mouseleave', e => {
-// 	e.preventDefault();
-// 	categories.style.position = 'fixed';
-// 	// video.style.marginTop = '5.6rem';
-// });
-
 categoriesUl.addEventListener('wheel', e => {
 	e.preventDefault();
 	categoriesUl.scrollLeft += e.deltaY;
-	// if (e) {
-	// 	categories.style.position = 'fixed';
-	// }
-	// categories.style.position = 'inherit';
 });
